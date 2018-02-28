7a945e2d5607d421e18f3afbaf2eae8542202b70
Pascal <scalblog@yahoo.fr>
2018-02-28 09:46:29 +0100

    Create texts-list-in-psd.js

---
+++ scripts/texts-list-in-psd.js
@@ -0,0 +1,17 @@
+/* Get the texts in a PSD file with this script in Photopea */
+
+var texts = [];
+getTexts(app.activeDocument, texts);
+console.log(texts);
+
+function getTexts(ref, fs) {
+    var len = ref.layers.length;
+    for (var i = 0; i < len; i++) {
+        var layer = ref.layers[i];
+        if (layer.typename=="LayerSet") getTexts(layer, fs);
+        else if(layer.kind==LayerKind.TEXT){
+            var text = layer.textItem.contents;
+            if(fs.indexOf(text)==-1) fs.push(text);
+        }
+    }
+}
